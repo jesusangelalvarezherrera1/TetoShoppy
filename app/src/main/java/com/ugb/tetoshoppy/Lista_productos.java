0222bc71a0d5715300b089bad5ba6211452c451d
jesusangelalvarezherrera1 <113858810+jesusangelalvarezherrera1@users.noreply.github.com>
2023-05-17 14:44:38 -0600

    Initial commit

---
+++ app/src/main/java/com/ugb/tetoshoppy/Lista_productos.java
@@ -0,0 +1,14 @@
+package com.ugb.tetoshoppy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lista_productos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lista_productos);
+    }
+}
---
+++ app/src/main/java/com/ugb/tetoshoppy/Login_Main.java
@@ -0,0 +1,14 @@
+package com.ugb.tetoshoppy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_main);
+    }
+}
